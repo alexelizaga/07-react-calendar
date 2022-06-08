698c23ef0f9b71fea844ee37bb6fc3441b79ed0b
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-08 08:12:57 +0200

    370

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,8 +1,26 @@
+import { fetchWithoutToken } from '../helpers/fetch';
+import { types } from '../types/types';
 
 export const authStartLogin = ( email, password ) => {
-    return async() => {
+    return async( dispatch ) => {
 
-        console.log(email, password);
+        const resp = await fetchWithoutToken( 'auth', { email, password }, 'POST' );
+        const body = await resp.json();
+
+        if ( body.ok ) {
+            localStorage.setItem( 'token', body.token );
+            localStorage.setItem( 'token-init-date', new Date().getTime() );
+
+            dispatch( login( {
+                uid: body.uid,
+                name: body.name,
+            } ) );
+        }
 
     }
 }
+
+const login = ( user ) => ({
+    type: types.authLogin,
+    payload: user
+})
