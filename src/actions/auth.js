9a0b5d6fe9e37638e9f5645451da5c03b3b7b3ae
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-08 09:43:03 +0200

    372

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,5 @@
 import Swal from 'sweetalert2';
-import { fetchWithoutToken } from '../helpers/fetch';
+import { fetchWithoutToken, fetchWithToken } from '../helpers/fetch';
 import { types } from '../types/types';
 
 export const authStartLogin = ( email, password ) => {
@@ -48,3 +48,27 @@ const login = ( user ) => ({
     type: types.authLogin,
     payload: user
 });
+
+export const authStartCheking  = () => {
+    return async( dispatch ) => {
+            
+        const resp = await fetchWithToken( 'auth/renew' );
+        const body = await resp.json();
+
+        if ( body.ok ) {
+            localStorage.setItem( 'token', body.token );
+            localStorage.setItem( 'token-init-date', new Date().getTime() );
+
+            dispatch( login( {
+                uid: body.uid,
+                name: body.name,
+            } ) );
+        } else {
+            Swal.fire('Error', body.msg, 'error');
+            dispatch( authCheckingFinish() );
+        }
+
+    }
+};
+
+const authCheckingFinish = () => ({ type: types.authCheckingFinish })
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { authStartCheking } from '../actions/auth';
 
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch( authStartCheking() )
+  }, [dispatch])
+  
   return (
     <BrowserRouter>
       <Routes>
