174f22342c3435f174eebbe6cb142cb7b488ac6e
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-08 07:32:27 +0200

    368

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,8 +1,13 @@
 
-const initialState = {}
+const initialState = {
+    checking: true,
+    // uid: null,
+    // name: null
+}
 
 export const authReducer = (state = initialState, action) => {
     switch (action.type) {
+        
         default:
             return state;
     }
--- src/types/types.js
+++ src/types/types.js
@@ -1,5 +1,13 @@
 export const types = {
 
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
     uiOpenModal: '[UI] Open modal',
     uiCloseModal: '[UI] Close modal',
 
