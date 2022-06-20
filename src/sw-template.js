bebc62f4e3bc58aa5e7578db8e7c67ca28452cbc
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-20 09:35:55 +0200

    203. Workbox Cache Strategies

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,3 +1,17 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
+
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+
+
+registerRoute(
+    new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
+    new CacheFirst()
+);
+
+registerRoute(
+    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'),
+    new CacheFirst()
+)
