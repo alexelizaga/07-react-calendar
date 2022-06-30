9babce8da3f53681924f99906c56db1f8bf7eeb7
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-30 11:11:31 +0200

    205. Background Sync - Post

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,13 +1,38 @@
+/* eslint-disable no-undef */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-// eslint-disable-next-line no-undef
 const baseUrl = 'https://bc-calendar.herokuapp.com/api';
+
+workbox.loadModule('workbox-background-sync');
 
 workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
+
+registerRoute(
+    new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
+    new CacheFirst()
+);
+
+registerRoute(
+    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'),
+    new CacheFirst()
+)
+
+registerRoute(
+    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/webfonts/fa-regular-400.woff2'),
+    new CacheFirst()
+)
+
+registerRoute(
+    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/webfonts/fa-regular-400.ttf'),
+    new CacheFirst()
+)
+
+// Offline Gets
 
 registerRoute(
     new RegExp(`${baseUrl}/auth/renew` ),
@@ -19,12 +44,16 @@ registerRoute(
     new NetworkFirst()
 )
 
-registerRoute(
-    new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
-    new CacheFirst()
-);
+// Offline Posts
+
+const bgSyncPlugin = new BackgroundSyncPlugin('offlinePosts', {
+    maxRetentionTime: 24 * 60,
+});
 
 registerRoute(
-    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'),
-    new CacheFirst()
+    new RegExp(`${baseUrl}/events`),
+    new NetworkOnly({
+        plugins: [ bgSyncPlugin ]
+    }),
+    'POST'
 )
