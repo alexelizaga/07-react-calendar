4b8b5c409f6ea3de2712dc6b159109ebe4439b2f
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-30 17:36:34 +0200

    207. Service worker optimization

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,48 +1,50 @@
 /* eslint-disable no-undef */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
-const baseUrl = 'https://bc-calendar.herokuapp.com/api';
 
 workbox.loadModule('workbox-background-sync');
 
 workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
 
+const baseUrl = 'https://bc-calendar.herokuapp.com/api';
 const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
 const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
+if (workbox) {
+    console.log(`Workbox is loaded 🎉`);
+} else {
+    console.log(`Workbox didn't load `);
+}
+
+const cacheCacheFirst = [
+    'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
+];
 
 registerRoute(
-    new RegExp('https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'),
+    ({ request, url }) => {
+        if( cacheCacheFirst.includes( url.href ) ) return true;
+        return false;
+    },
     new CacheFirst()
 );
 
-registerRoute(
-    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'),
-    new CacheFirst()
-)
-
-registerRoute(
-    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/webfonts/fa-regular-400.woff2'),
-    new CacheFirst()
-)
-
-registerRoute(
-    new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/webfonts/fa-regular-400.ttf'),
-    new CacheFirst()
-)
-
 // Offline Gets
 
-registerRoute(
-    new RegExp(`${baseUrl}/auth/renew` ),
-    new NetworkFirst()
-)
+const cacheNetworkFirst = [
+    '/api/auth/renew',
+    '/api/events',
+]
 
 registerRoute(
-    new RegExp(`${baseUrl}/events` ),
+    ({ request, url }) => {
+        if( cacheNetworkFirst.includes( url.pathname ) ) return true;
+        return false;
+    },
     new NetworkFirst()
-)
+);
+
 
 // Offline Posts
 
@@ -56,4 +58,20 @@ registerRoute(
         plugins: [ bgSyncPlugin ]
     }),
     'POST'
-)
+);
+
+registerRoute(
+    new RegExp(`${baseUrl}/events/`),
+    new NetworkOnly({
+        plugins: [ bgSyncPlugin ]
+    }),
+    'DELETE'
+);
+
+registerRoute(
+    new RegExp(`${baseUrl}/events/`),
+    new NetworkOnly({
+        plugins: [ bgSyncPlugin ]
+    }),
+    'PUT'
+);
